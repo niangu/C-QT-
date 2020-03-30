ca8e76b42a48ff6f52df189e8c3101fdf694f67a
念古 <45702522+niangu@users.noreply.github.com>
2020-03-31 07:55:40 +0800

    Create main.cpp

---
+++ magicfish/main.cpp
@@ -0,0 +1,31 @@
+#include "magicpool.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setFont(QFont("Microsoft Yahei", 9));
+
+#if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
+#if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+#endif
+
+    MagicPool w;    
+    w.setWindowFlags(Qt::FramelessWindowHint);
+    w.setAttribute(Qt::WA_TranslucentBackground, true);
+    //w.resize(1000, 700);
+    w.showFullScreen();
+
+    return a.exec();
+}
