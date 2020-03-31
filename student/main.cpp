84b11be34ea80fbdf0a7c1a5df792e4c23a345af
念古 <45702522+niangu@users.noreply.github.com>
2020-03-31 08:31:59 +0800

    Add files via upload

---
+++ student/main.cpp
@@ -0,0 +1,31 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include<QDebug>
+#include<QSqlDatabase>
+bool opendatabase();
+int main(int argc, char *argv[])
+{
+    opendatabase();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();//QQ765715928
+
+    return a.exec();
+}
+bool opendatabase()
+{
+    QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE");
+
+    mydb.setDatabaseName("D:\\sqlite\\student.db");//平时debug正常用
+    //mydb.setDatabaseName("./student.db");//release用
+    if(mydb.open())
+    {
+        qDebug()<<"open success";
+        return true;
+    }
+    else
+    {
+        qDebug()<<"open failed";
+        return false;
+    }
+}
